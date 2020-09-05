61fb8b9b8bc69d1e3e405ebe34c22633684b1572
nunopenim <nuno.penim@gmail.com>
2020-09-05 18:47:12 +0100

    general: added a dummy module, for testing later

---
+++ dummy.py
@@ -0,0 +1,7 @@
+from tg_userbot import tgclient
+from telethon.events import NewMessage
+
+@tgclient.on(NewMessage(pattern=r"^\.dummy$", outgoing=True))
+async def text(msg):
+    await msg.edit("Ping!")
+    return
