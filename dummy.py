7de1e1232c43086ff9cdf31d6cd69ba2766d127c
nunopenim <nuno.penim@gmail.com>
2020-09-08 13:16:16 +0100

    dummy: package name changed

--- dummy.py
+++ dummy.py
@@ -1,4 +1,4 @@
-from userbot import tgclient, MODULE_DESC, MODULE_DICT
+from userbot import tgclient
 from telethon.events import NewMessage
 
 @tgclient.on(NewMessage(pattern=r"^\.dummy$", outgoing=True))
@@ -6,5 +6,3 @@ async def text(msg):
     await msg.edit("Ping!")
     return
 
-MODULE_DESC.update({basename(__file__)[:-3]: "Dummy module designed during testing"})
-MODULE_DICT.update({basename(__file__)[:-3]: ".dummy with print 'ping' to see if it is working"})
