5ed64d17c44d303780d64f8262e8ebbbf45e99ad
nunopenim <nuno.penim@gmail.com>
2020-09-10 12:24:34 +0100

    general: updated README, added python module

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Copyright (C) 2020 Nuno Penim
 
 All rights and lefts reserved, under construction.
 
+# Hosting your own community repo
+The package manager system of the bot runs using my [pyGitHub_API](https://github.com/nunopenim/pyGitHub_API) library. This library can only see the Releases of a GitHub repo, so to host your own, you need to create a release. A full tutorial will come out soon enough. It is recommended that to add a file to the repo, you edit the release, instead of creating a new one.
+
 # Licensing
 
 Unless specified on the header of the file, all the components of this repo are licensed under [PEL](https://github.com/nunopenim/module-universe/blob/master/LICENSE.md). A copy of all the Licenses included in this repo can be found under the "Licenses" directory.
---
+++ python.py
@@ -0,0 +1,32 @@
+# Copyright 2020 nunopenim @github
+# Copyright 2020 prototype74 @github
+#
+# Licensed under the PEL (Penim Enterprises License), v1.0
+#
+# You may not use this file or any of the content within it, unless in
+# compliance with the PE License
+
+from userbot import tgclient, MODULE_DESC, MODULE_DICT
+from telethon.events import NewMessage
+from subprocess import check_output, CalledProcessError
+from os.path import basename
+from sys import executable
+
+@tgclient.on(NewMessage(pattern=r"^\.python(?: |$)(.*)", outgoing=True))
+async def python(command):
+    python_instruction = command.pattern_match.group(1).split(" ", 1)
+    command_for_bash = executable + " -c " + '"' + python_instruction + '"'
+    try:
+        cmd_output = check_output(command_for_bash, shell=True).decode()
+    except CalledProcessError:
+        cmd_output = "Error executing instruction! Most likely you used \" instead of '. This is a known issue."
+    output = "**Python instruction:** `" + python_instruction + "`\n\n"
+    output +=  "**Result: **\n`" + cmd_output + "`"
+    await command.edit(output)
+    return
+
+DESCRIPTION = "This official add-on module is a Python interpreter. You can run small instructions."
+USAGE = "´.python´ <instruction(s)>\nUsage: Runs the specified python instruction."
+
+MODULE_DESC.update({basename(__file__)[:-3]: DESCRIPTION})
+MODULE_DICT.update({basename(__file__)[:-3]: USAGE})
